b233eb3d2573d5765f9b1c0f31a54b5420afa718
JamaalHarris <jamaalmalikharris@gmail.com>
2023-05-10 19:40:05 -0500

    initial commit

--- src/Content.jsx
+++ src/Content.jsx
@@ -1,7 +1,13 @@
+import { Signup } from "./Signup";
+import { Login } from "./Login";
+
 export function Content() {
   return (
     <div>
-      <h1>Welcome to React!</h1>
+      <h1>Nutri-Calc!</h1>
+      <h2>The Nutritional Calculator</h2>
+      <Signup />
+      <Login />
     </div>
   );
 }
---
+++ src/FavoritesIndex.jsx
@@ -0,0 +1,7 @@
+export function FavoritesIndex() {
+  return (
+    <div>
+      <h1>All favorites</h1>
+    </div>
+  );
+}
